2112a9d4300fe9694a74b3844d4c6fe5342f0a2e
killiadmin <k.filatre@gmail.com>
2023-09-07 15:30:36 +0200

    add specified has return my functions in models comments for symfony insight

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -17,7 +17,7 @@ class Comment
         $this->hydrate($data);
     }
 
-    public function hydrate (array $data)
+    public function hydrate (array $data): void
     {
         foreach ($data as $key => $value) {
             $method = 'set'.ucfirst($key);
@@ -28,7 +28,7 @@ class Comment
     }
 
     //setters
-    public function setIdComment(int $idComment)
+    public function setIdComment(int $idComment): void
     {
         $idComment = (int) $idComment;
         if ($idComment > 0) {
@@ -36,45 +36,45 @@ class Comment
         }
     }
 
-    public function setIdUserAssociated(int $idUserAssociated)
+    public function setIdUserAssociated(int $idUserAssociated): void
     {
         if ((int) ($idUserAssociated)) {
             $this->_idUserAssociated = $idUserAssociated;
         }
     }
 
-    public function setIdPostAssociated(int $idPostAssociated)
+    public function setIdPostAssociated(int $idPostAssociated): void
     {
         if ((int) ($idPostAssociated)) {
             $this->_idPostAssociated = $idPostAssociated;
         }
     }
 
-    public function setContent(string $content)
+    public function setContent(string $content): void
     {
         if (is_string($content)) {
             $this->_content = $content;
         }
     }
 
-    public function setDateCreate(string $dateCreate)
+    public function setDateCreate(string $dateCreate): void
     {
         $this->_dateCreate = $dateCreate;
     }
 
-    public function setDateUpdate(string $dateUpdate)
+    public function setDateUpdate(string $dateUpdate): void
     {
         $this->_dateUpdate = $dateUpdate;
     }
 
-    public function setName(string $name)
+    public function setName(string $name): void
     {
         if (is_string($name)) {
             $this->_name = $name;
         }
     }
 
-    public function setUserName(string $userName)
+    public function setUserName(string $userName): void
     {
         if (is_string($userName)) {
             $this->_userName = $userName;
@@ -83,42 +83,50 @@ class Comment
 
     //getters
 
-    public function idComment()
+    public function idComment(): int
     {
         return $this->_idComment;
     }
 
-    public function idUserAssociated()
+    public function idUserAssociated(): int
     {
         return $this->_idUserAssociated;
     }
 
-    public function idPostAssociated()
+    public function idPostAssociated(): int
     {
         return $this->_idPostAssociated;
     }
 
-    public function content()
+    public function content(): string
     {
         return $this->_content;
     }
 
-    public function dateCreate()
+    public function dateCreate(): string
     {
-        return $this->_dateCreate;
+        $dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $this->_dateCreate);
+        if ($dateTime instanceof DateTime) {
+            return $dateTime->format('Y-m-d H:i:s');
+        }
+        return '';
     }
 
-    public function dateUpdate()
+    public function dateUpdate(): string
     {
-        return $this->_dateUpdate;
+        $dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $this->_dateUpdate);
+        if ($dateTime instanceof DateTime) {
+            return $dateTime->format('Y-m-d H:i:s');
+        }
+        return '';
     }
 
-    public function name()
+    public function name(): string
     {
         return$this->_name;
     }
 
-    public function userName()
+    public function userName(): string
     {
         return$this->_userName;
     }
