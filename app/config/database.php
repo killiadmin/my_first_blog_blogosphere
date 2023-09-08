bfcd65537bb18c3bbba3dbe6cd99dc15821e0116
killiadmin <k.filatre@gmail.com>
2023-09-08 22:37:32 +0200

    add file database for use connection db separated and connection params

---
+++ app/config/database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '8889');
+define('DB_NAME', 'u746425507_blogorama');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'azerty');
---
+++ app/models/DatabaseConnection.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Method for connect to Database
+ */
+
+class DatabaseConnection
+{
+    public static function setConnection(): PDO
+    {
+        require('./app/config/database.php');
+        $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+
+        try {
+            $db = new PDO($dsn, DB_USER, DB_PASSWORD);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $db;
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+}
