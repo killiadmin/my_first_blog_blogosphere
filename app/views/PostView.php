4bb471a5418158afe608d15be4b6d29ab19bd7e2
killiadmin <k.filatre@gmail.com>
2023-09-11 20:48:24 +0200

    add message if array posts is empty

--- app/views/PostView.php
+++ app/views/PostView.php
@@ -2,8 +2,17 @@
 $postCounter = 0;
 ?>
 
-<div class="mx-auto" style="max-width: 1000px;"">
-    <?php foreach ($posts ?? [] as $post):
+<div class="mx-auto" style="max-width: 1000px;">
+    <?php if (empty($posts)) {
+        ?>
+        <div class="container d-flex flex-column align-items-center gap-3 m-5">
+            <h4 class="display-4">No articles are posted at the moment</h4>
+            <p>You can return to my <a href="/singleuser&id=1" class="text-decoration-underline"
+                                       style="color: #3C4245;">home</a>.</p>
+        </div>
+        <?php
+    }
+    foreach ($posts ?? [] as $post):
 
         $dateFormat = "d/m/Y H:i:s";
         $timeStamp = strtotime($post->dateUpdate());
@@ -22,11 +31,14 @@ $postCounter = 0;
                     <p class="card-text"><?= htmlspecialchars($post->chapo()) ?></p>
                     <div class="d-flex flex-column">
                         <p>&nbsp;Modified the : <strong><?= htmlspecialchars($formatDateFr) ?></strong></p>
-                        <p>Author : <strong><?= htmlspecialchars($post->name()) ?> <?= htmlspecialchars($post->username()) ?></strong>&nbsp;</p>
+                        <p>Author :
+                            <strong><?= htmlspecialchars($post->name()) ?> <?= htmlspecialchars($post->username()) ?></strong>&nbsp;
+                        </p>
                     </div>
                 </div>
                 <div class="d-flex flex-row-reverse">
-                    <a href="/singlepost&id=<?= htmlspecialchars($post->idPost()) ?>" class="btn btn-secondary">Go somewhere</a>
+                    <a href="/singlepost&id=<?= htmlspecialchars($post->idPost()) ?>" class="btn btn-secondary">Go
+                        somewhere</a>
                 </div>
             </div>
         </div>
